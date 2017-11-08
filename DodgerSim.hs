ecaca6697ac6305ebb847661d9d3b53ef8c6c5a8
richardbowmaker <rjb.accounts@live.co.uk>
2017-11-08 17:24:25 +0000

    start of day backup

--- DodgerSim.hs
+++ DodgerSim.hs
@@ -1,6 +1,7 @@
 module Main where
 import Graphics.UI.WX
 import System.Random
+import Data.List
 
 -- size of display
 maxX, maxY :: Int
@@ -55,41 +56,59 @@ generateMoves :: [[Dodger]] -> [[Dodger]]
 --generateMoves _ = restOfMoves [[(Dodger "1" (Point 0 50) (Point 5 0) 10)]] (mkStdGen 100)       
 generateMoves _ = restOfMoves [] (mkStdGen 100)       
 
+-- generates an list of lists comprising all the dodgers and their moves as an infinite list.
+-- the outer list is a time array, t0, t1, t2 etc with each element being a list of dodgers and there current position at
+-- time tn. 
+-- The dodgers are are added to the timeline randomly and will move either vertically or horizontally
+
+
+
 restOfMoves :: [[Dodger]] -> StdGen -> [[Dodger]]
-restOfMoves [] gen = case new of 
-                        Nothing -> restOfMoves [] gen'
-                        Just d ->  [[d]] ++ restOfMoves [[d]] gen'
-        where  (gen', new) = genRandDodger gen
-        
-restOfMoves xs gen = case new of 
-                        Nothing -> [nextMoves] ++ restOfMoves [nextMoves] gen'
-                        Just d ->  [d:nextMoves] ++ restOfMoves [d:nextMoves] gen'
-        where   currentMoves = last xs
-                nextMoves = filter inDisplay (map dodgerMove currentMoves)
-                dodgerMove :: Dodger -> Dodger    
-                dodgerMove (Dodger id (Point x y) speed@(Point vx vy) radius) =
-                            (Dodger id (Point (x+vx) (y+vy)) speed radius)
-                inDisplay :: Dodger -> Bool
-                inDisplay (Dodger _ (Point x y) _ _) = (x < maxX) && (y < maxY)
-                (gen', new) = genRandDodger gen
+restOfMoves xs gen = [moves] ++ restOfMoves [moves] gen'
+
+            where   currentMoves = case xs of
+                        [] -> []
+                        xs -> last xs
                     
-genRandDodger :: StdGen -> (StdGen, Maybe Dodger)    
-genRandDodger gen = if new == 1
-                    then
-                        if isX
-                        then
-                            (gen5, Just (Dodger "X" (Point 0 start) (Point speed 0) radius))
-                        else
-                            (gen5, Just (Dodger "Y" (Point start 0) (Point 0 speed) radius))
-                    else
-                        (gen1, Nothing)
-                    where
-                        (new, gen1)     = randomR (1,10)   gen  :: (Int, StdGen)
-                        (radius, gen2)  = randomR (10,20)  gen1 :: (Int, StdGen)
-                        (isX, gen3)     = random           gen2 :: (Bool, StdGen)
-                        (start, gen4)   = randomR (1,maxX) gen3 :: (Int, StdGen)
-                        (speed, gen5)   = randomR (1,20)    gen4 :: (Int, StdGen)
+                    -- update all dodger postitions and discard any that have moved outside
+                    -- the display area 
+                    updatedMoves = filter inDisplay (map dodgerMove currentMoves)
+                 
+                    -- update the position of the dodger
+                    dodgerMove :: Dodger -> Dodger    
+                    dodgerMove (Dodger id (Point x y) speed@(Point vx vy) radius) =
+                                (Dodger id (Point (x+vx) (y+vy)) speed radius)
+                     
+                    -- is the dodger still in the display area
+                    inDisplay :: Dodger -> Bool
+                    inDisplay (Dodger _ (Point x y) _ _) = (x < maxX) && (y < maxY)
                     
+                    -- generate new dodger and insert into list of dodgers 
+                    (gen', md) = genRandDodger gen
+                    moves = case md of
+                        Nothing -> updatedMoves
+                        Just d  -> d:updatedMoves
+                       
+                    -- generate a random doger (10%) or nothing (90%)
+                    genRandDodger :: StdGen -> (StdGen, Maybe Dodger)    
+                    genRandDodger gen = if new == 1
+                                        then
+                                            if isX
+                                            then
+                                                (gen5, Just (Dodger "X" (Point 0 start) (Point speed 0) radius))
+                                            else
+                                                (gen5, Just (Dodger "Y" (Point start 0) (Point 0 speed) radius))
+                                        else
+                                            (gen1, Nothing)
+                                        where
+                                            (new, gen1)     = randomR (1,10)   gen  :: (Int, StdGen)
+                                            (radius, gen2)  = randomR (10,20)  gen1 :: (Int, StdGen)
+                                            (isX, gen3)     = random           gen2 :: (Bool, StdGen)
+                                            (start, gen4)   = randomR (1,maxX) gen3 :: (Int, StdGen)
+                                            (speed, gen5)   = randomR (1,20)   gen4 :: (Int, StdGen)
                     
-                            
+                                        
+                        
+
+
                      
